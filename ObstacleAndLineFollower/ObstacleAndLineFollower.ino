70fca38137a0ecb644ed5a368b18d27637eac37b
TriDecent <116976929+TriDecent@users.noreply.github.com>
2024-11-26 12:43:52 +0700

    Refactor variable names for clarity in obstacle and line follower robot code

--- ObstacleAndLineFollower/ObstacleAndLineFollower.ino
+++ ObstacleAndLineFollower/ObstacleAndLineFollower.ino
@@ -1,28 +1,28 @@
 #include <Servo.h>
 
-const int TRIG_PIN = 3;
-const int ECHO_PIN = 4;
+const int ULTRASONIC_TRIGGER_PIN = 3;
+const int ULTRASONIC_ECHO_PIN = 4;
 const int MOTOR1_PIN1 = 6;
 const int MOTOR1_PIN2 = 7;
 const int MOTOR2_PIN1 = 8;
 const int MOTOR2_PIN2 = 9;
-const int SENSOR_LEFT = 2;
-const int SENSOR_MIDDLE = 4;
-const int SENSOR_RIGHT = 10;
+const int LINE_FOLLOWING_SENSOR_LEFT = 2;
+const int LINE_FOLLOWING_SENSOR_MIDDLE = 4;
+const int LINE_FOLLOWING_SENSOR_RIGHT = 10;
 
 Servo servo;
 
 void setup()
 {
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
+  pinMode(ULTRASONIC_TRIGGER_PIN, OUTPUT);
+  pinMode(ULTRASONIC_ECHO_PIN, INPUT);
   pinMode(MOTOR1_PIN1, OUTPUT);
   pinMode(MOTOR1_PIN2, OUTPUT);
   pinMode(MOTOR2_PIN1, OUTPUT);
   pinMode(MOTOR2_PIN2, OUTPUT);
-  pinMode(SENSOR_LEFT, INPUT);
-  pinMode(SENSOR_MIDDLE, INPUT);
-  pinMode(SENSOR_RIGHT, INPUT);
+  pinMode(LINE_FOLLOWING_SENSOR_LEFT, INPUT);
+  pinMode(LINE_FOLLOWING_SENSOR_MIDDLE, INPUT);
+  pinMode(LINE_FOLLOWING_SENSOR_RIGHT, INPUT);
 
   Serial.begin(9600);
   servo.attach(10);
@@ -30,13 +30,13 @@ void setup()
 
 long calculateUltrasonicDistance()
 {
-  digitalWrite(TRIG_PIN, LOW);
+  digitalWrite(ULTRASONIC_TRIGGER_PIN, LOW);
   delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
+  digitalWrite(ULTRASONIC_TRIGGER_PIN, HIGH);
   delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
+  digitalWrite(ULTRASONIC_TRIGGER_PIN, LOW);
 
-  long duration = pulseIn(ECHO_PIN, HIGH);
+  long duration = pulseIn(ULTRASONIC_ECHO_PIN, HIGH);
   return (duration * 0.034 / 2);
 }
 
@@ -111,9 +111,9 @@ void followLine(int leftLineFollowerValue, int middleLineFollowerValue, int righ
 void loop()
 {
   long distanceToObstacle = calculateUltrasonicDistance();
-  int leftLineFollowerSensorValue = digitalRead(SENSOR_LEFT);
-  int middleLineFollowerSensorValue = digitalRead(SENSOR_MIDDLE);
-  int rightLineFollowerSensorValue = digitalRead(SENSOR_RIGHT);
+  int leftLineFollowerSensorValue = digitalRead(LINE_FOLLOWING_SENSOR_LEFT);
+  int middleLineFollowerSensorValue = digitalRead(LINE_FOLLOWING_SENSOR_MIDDLE);
+  int rightLineFollowerSensorValue = digitalRead(LINE_FOLLOWING_SENSOR_RIGHT);
 
   Serial.print("Distance: ");
   Serial.print(distanceToObstacle);
