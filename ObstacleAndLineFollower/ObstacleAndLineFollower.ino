609a9555aec7cbf3dcc9a83ebbdda798037966e3
TriDecent <116976929+TriDecent@users.noreply.github.com>
2024-12-05 17:06:28 +0700

    Refactor obstacle avoidance logic and improve distance measurement handling

--- ObstacleAndLineFollower/ObstacleAndLineFollower.ino
+++ ObstacleAndLineFollower/ObstacleAndLineFollower.ino
@@ -11,14 +11,9 @@ const int LEFT_MOTOR_IN3 = 8;
 const int LEFT_MOTOR_IN4 = 7;
 const int LEFT_MOTOR_ENB = 5;
 
-const int LINE_FOLLOWING_SENSOR_LEFT = 2;
-const int LINE_FOLLOWING_SENSOR_MIDDLE = 4;
-const int LINE_FOLLOWING_SENSOR_RIGHT = 10;
-
 Servo servo;
 
-void setup()
-{
+void setup() {
   pinMode(ULTRASONIC_TRIGGER_PIN, OUTPUT);
   pinMode(ULTRASONIC_ECHO_PIN, INPUT);
   pinMode(RIGHT_MOTOR_IN1, OUTPUT);
@@ -27,57 +22,69 @@ void setup()
   pinMode(LEFT_MOTOR_IN3, OUTPUT);
   pinMode(LEFT_MOTOR_IN4, OUTPUT);
   pinMode(LEFT_MOTOR_ENB, OUTPUT);
-  pinMode(LINE_FOLLOWING_SENSOR_LEFT, INPUT);
-  pinMode(LINE_FOLLOWING_SENSOR_MIDDLE, INPUT);
-  pinMode(LINE_FOLLOWING_SENSOR_RIGHT, INPUT);
 
   Serial.begin(9600);
   servo.attach(3);
   setServoToCenter();
 }
 
-void loop()
-{
+void loop() {
   long distanceToObstacle = calculateUltrasonicDistance();
-  int leftLineFollowerSensorValue = digitalRead(LINE_FOLLOWING_SENSOR_LEFT);
-  int middleLineFollowerSensorValue = digitalRead(LINE_FOLLOWING_SENSOR_MIDDLE);
-  int rightLineFollowerSensorValue = digitalRead(LINE_FOLLOWING_SENSOR_RIGHT);
+  Serial.print("Distance to Obstacle: ");
+  Serial.println(distanceToObstacle);
 
-  Serial.print("Distance: ");
-  Serial.print(distanceToObstacle);
-  Serial.print(" cm, Left: ");
-  Serial.print(leftLineFollowerSensorValue);
-  Serial.print(", Middle: ");
-  Serial.print(middleLineFollowerSensorValue);
-  Serial.print(", Right: ");
-  Serial.println(rightLineFollowerSensorValue);
-
-  if (distanceToObstacle > 0 && distanceToObstacle < 20)
-  {
-    avoidObstacle();
-  }
-  else
-  {
-    setServoToCenter();
-    moveForwardWithSpeed(255);
+  if (distanceToObstacle > 30 || distanceToObstacle == 0) {
+    moveForwardWithSpeed(200);
     Serial.println("Moving forward");
+  } else {
+    stopCar(); 
+    delay(300);
+    long distanceLeft = turnLeftAndMeasureDistance();
+    long distanceRight = turnRightAndMeasureDistance();
+
+    if (distanceLeft < 15 && distanceRight < 15) {
+      moveBackwardWithSpeed(200);
+      delay(300);
+      stopCar();
+      Serial.println("Moving backward");
+    } else {
+      if (distanceRight >= distanceLeft) {
+        moveBackwardWithSpeed(200);
+        delay(300);
+        stopCar();
+        delay(300);
+        turnLeftWithSpeed(200);
+        delay(350);
+        stopCar();
+        delay(300);
+        Serial.println("Turning right");
+      } else if (distanceRight < distanceLeft) {
+        moveBackwardWithSpeed(200);
+        delay(300);
+        stopCar();
+        delay(300);
+        turnRightWithSpeed(200);
+        delay(350);
+        stopCar();
+        delay(300);
+        Serial.println("Turning left");
+      }
+    }
   }
 
   delay(100);
 }
 
-long calculateUltrasonicDistance()
-{
+long calculateUltrasonicDistance() {
   digitalWrite(ULTRASONIC_TRIGGER_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(ULTRASONIC_TRIGGER_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(ULTRASONIC_TRIGGER_PIN, LOW);
 
-  long duration = pulseIn(ULTRASONIC_ECHO_PIN, HIGH, 30000);
+  long duration = pulseIn(ULTRASONIC_ECHO_PIN, HIGH, 20000);
 
-  if (duration == 0)
-  {
+  if (duration == 0) {
     Serial.println("No obstacle detected (timeout).");
     return 999;
   }
@@ -85,85 +92,27 @@ long calculateUltrasonicDistance()
   return duration * 0.034 / 2;
 }
 
-void avoidObstacle()
-{
-  stopCar();
-
-  long distanceFront = checkDistanceAtAngle(90);
-  long distanceLeft = checkDistanceAtAngle(45);
-  long distanceRight = checkDistanceAtAngle(135);
-
-  Serial.print("Distances - Front: ");
-  Serial.print(distanceFront);
-  Serial.print(" cm, Left: ");
-  Serial.print(distanceLeft);
-  Serial.print(" cm, Right: ");
-  Serial.println(distanceRight);
-
-  if (distanceFront > 20)
-  {
-    moveForwardWithSpeed(200);
-    delay(300);
-  }
-  else if (distanceLeft > distanceRight && distanceLeft > 15)
-  {
-    turnLeftWithSpeed(200);
-    delay(500);
-  }
-  else if (distanceRight > distanceLeft && distanceRight > 15)
-  {
-    turnRightWithSpeed(200);
-    delay(500);
-  }
-  else
-  {
-    moveBackwardWithSpeed(200);
-    delay(300);
-    turnLeftWithSpeed(200);
-    delay(500);
-  }
-
-  stopCar();
-}
-
-long checkDistanceAtAngle(int angle)
-{
-  servo.write(angle);
+long turnLeftAndMeasureDistance() {
+  servo.write(0);
   delay(500);
   long distance = calculateUltrasonicDistance();
   setServoToCenter();
   return distance;
 }
 
-void setServoToCenter()
-{
+long turnRightAndMeasureDistance() {
+  servo.write(180);
+  delay(500);
+  long distance = calculateUltrasonicDistance();
+  setServoToCenter();
+  return distance;
+}
+
+void setServoToCenter() {
   servo.write(90);
 }
 
-void moveForwardWithSpeed(int speed)
-{
-  digitalWrite(RIGHT_MOTOR_IN1, HIGH);
-  digitalWrite(RIGHT_MOTOR_IN2, LOW);
-  analogWrite(RIGHT_MOTOR_ENA, speed);
-
-  digitalWrite(LEFT_MOTOR_IN3, HIGH);
-  digitalWrite(LEFT_MOTOR_IN4, LOW);
-  analogWrite(LEFT_MOTOR_ENB, speed);
-}
-
-void moveBackwardWithSpeed(int speed)
-{
-  digitalWrite(RIGHT_MOTOR_IN1, LOW);
-  digitalWrite(RIGHT_MOTOR_IN2, HIGH);
-  analogWrite(RIGHT_MOTOR_ENA, speed);
-
-  digitalWrite(LEFT_MOTOR_IN3, LOW);
-  digitalWrite(LEFT_MOTOR_IN4, HIGH);
-  analogWrite(LEFT_MOTOR_ENB, speed);
-}
-
-void turnLeftWithSpeed(int speed)
-{
+void moveForwardWithSpeed(int speed) {
   digitalWrite(RIGHT_MOTOR_IN1, HIGH);
   digitalWrite(RIGHT_MOTOR_IN2, LOW);
   analogWrite(RIGHT_MOTOR_ENA, speed);
@@ -173,8 +122,7 @@ void turnLeftWithSpeed(int speed)
   analogWrite(LEFT_MOTOR_ENB, speed);
 }
 
-void turnRightWithSpeed(int speed)
-{
+void moveBackwardWithSpeed(int speed) {
   digitalWrite(RIGHT_MOTOR_IN1, LOW);
   digitalWrite(RIGHT_MOTOR_IN2, HIGH);
   analogWrite(RIGHT_MOTOR_ENA, speed);
@@ -184,8 +132,27 @@ void turnRightWithSpeed(int speed)
   analogWrite(LEFT_MOTOR_ENB, speed);
 }
 
-void stopCar()
-{
+void turnLeftWithSpeed(int speed) {
+  digitalWrite(RIGHT_MOTOR_IN1, HIGH);
+  digitalWrite(RIGHT_MOTOR_IN2, LOW);
+  analogWrite(RIGHT_MOTOR_ENA, speed);
+
+  digitalWrite(LEFT_MOTOR_IN3, HIGH);
+  digitalWrite(LEFT_MOTOR_IN4, LOW);
+  analogWrite(LEFT_MOTOR_ENB, speed);
+}
+
+void turnRightWithSpeed(int speed) {
+  digitalWrite(RIGHT_MOTOR_IN1, LOW);
+  digitalWrite(RIGHT_MOTOR_IN2, HIGH);
+  analogWrite(RIGHT_MOTOR_ENA, speed);
+
+  digitalWrite(LEFT_MOTOR_IN3, LOW);
+  digitalWrite(LEFT_MOTOR_IN4, HIGH);
+  analogWrite(LEFT_MOTOR_ENB, speed);
+}
+
+void stopCar() {
   digitalWrite(RIGHT_MOTOR_IN1, LOW);
   digitalWrite(RIGHT_MOTOR_IN2, LOW);
   analogWrite(RIGHT_MOTOR_ENA, 0);
